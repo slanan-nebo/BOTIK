d773ef5c65beb75301e52dc5603b2ca9ea09cabf
Mmarggit <92600481+Mmarggit@users.noreply.github.com>
2022-11-17 22:51:40 +0300

    Matrix class added

--- matrix.py
+++ matrix.py
@@ -17,10 +17,11 @@ class Matrix:
         return res
 
 
-mat = Matrix(3, 3, 'SLAVA')
-print(mat.show())
+"""mat = Matrix(3, 3, 'SLAVA')
+print(mat.show())"""
 
 c = [[1, 2, 3], [0, 5, 6], [0, 0, 9]]
+
 
 def linear(some_list):
     if not some_list:
@@ -30,9 +31,31 @@ def linear(some_list):
     return some_list[:1] + linear(some_list[1:])
 
 
+def transpon(c):
+    ct = list(map(list, zip(*c)))
+    return ct
+
+
+def support_matrix(matrix: list, row: int, col: int):
+    print(matrix)
+    matrix.pop(row)
+    matrix = transpon(matrix)
+    matrix.pop(col)
+    matrix = transpon(matrix)
+    print(matrix)
+    return matrix
+
+
 def det(c: list):
     if len(linear(c)) == 1:
         return int(linear([[0]])[0])
+    if len(linear(c)) == 4:
+        return c[0][0] * c[1][1] - c[1][0] * c[0][1]
     ans = 0
-    for i in range(3):
-        for j in range(3):
+    row = 0
+    for col in range(len(c)):
+        ans += ((-1) ** (row + col + 1)) * c[row][col] * det(support_matrix(c, row, col))
+        print(f'===>{ans}')
+    return ans
+
+print(det(c))
