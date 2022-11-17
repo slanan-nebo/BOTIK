7d6d411ad6191fc3ed5aa7f582387b4de388cde5
Mmarggit <92600481+Mmarggit@users.noreply.github.com>
2022-11-17 22:04:34 +0300

    Matrix class added

--- matrix.py
+++ matrix.py
@@ -19,3 +19,20 @@ class Matrix:
 
 mat = Matrix(3, 3, 'SLAVA')
 print(mat.show())
+
+c = [[1, 2, 3], [0, 5, 6], [0, 0, 9]]
+
+def linear(some_list):
+    if not some_list:
+        return some_list
+    if type(some_list[0]) is list:
+        return linear(some_list[0]) + linear(some_list[1:])
+    return some_list[:1] + linear(some_list[1:])
+
+
+def det(c: list):
+    if len(linear(c)) == 1:
+        return int(linear([[0]])[0])
+    ans = 0
+    for i in range(3):
+        for j in range(3):
